dd1fd09335f6fed5e6d9027b381a8fc4a91bde18
anakobra159 <oruchan.asar@gmail.com>
2021-02-20 10:35:05 +0300

    fixed size of picutre

--- script.js
+++ script.js
@@ -2,10 +2,10 @@ var image = document.getElementById("image");
 
 image.addEventListener("mouseover", function(){
     this.style = "box-shadow: 2px 2px 2px grey";
-    this.width = "160";
+    this.width = "160px";
 });
 
 image.addEventListener("mouseout", function(){
     this.style = ""
-    this.width = "150"
+    this.width = "150px"
 });
