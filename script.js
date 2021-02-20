c9113e7d84582287b1b23e6effce70b1608163e3
anakobra159 <oruchan.asar@gmail.com>
2021-02-20 10:00:41 +0300

    JavaScript changed

--- script.js
+++ script.js
@@ -2,5 +2,10 @@ var image = document.getElementById("image");
 
 image.addEventListener("mouseover", function(){
     this.style = "box-shadow: 2px 2px 2px grey";
-    this.width = "100";
+    this.width = "110";
+});
+
+image.addEventListener("mouseout", function(){
+    this.style = ""
+    this.width = ""
 });
