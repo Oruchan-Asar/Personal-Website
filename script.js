b030b83594c60cfadabe332dbc1cdafaeb21af47
anakobra159 <oruchan.asar@gmail.com>
2021-02-26 09:45:07 +0300

    added dark theme

--- script.js
+++ script.js
@@ -1,11 +1,16 @@
 var image = document.getElementById("image");
+const toggle = document.getElementById("toggle");
 
 image.addEventListener("mouseover", function(){
     this.style = "box-shadow: 2px 2px 2px grey";
-    this.width = "160px";
+    this.width = "180px";
 });
 
 image.addEventListener("mouseout", function(){
     this.style = ""
     this.width = "150px"
+});
+
+toggle.addEventListener("change", (e) => {
+    document.body.classList.toggle("dark", e.target.checked);
 });
